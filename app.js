6539005103b808406f2f9d012c3b66d4f3f787ec
dimas firdaus <maz.dimas@gmail.com>
2020-04-17 18:58:38 +0700

    start project

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT | 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello from ExpressJS');
+});
+
+app.get('/about', (req, res) => {
+    res.send('About page');
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
+
