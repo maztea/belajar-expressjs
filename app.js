f21eb733bdf354df7f9467fb5f94c914aa2ad30a
Dimas Firdaus <dimas@fungsitama.com>
2020-04-18 20:41:15 +0700

    test dari windows

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT | 3000;
 
-const contact = require('./routes/contact')
+const contact = require("./routes/contact");
 
-app.use('/contact', contact);
+app.use("/contact", contact);
 
-app.get('/', (req, res) => {
-    res.send('Hello from ExpressJS..');
+app.get("/", (req, res) => {
+  res.send("Hello from ExpressJS");
 });
 
-app.get('/about', (req, res) => {
-    res.send('About page..');
+app.get("/about", (req, res) => {
+  res.send("About page..");
 });
 
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+  console.log(`Server started on port ${port}`);
 });
-
